34a18e95b2bc7685d86379dac2daf5385e281ba0
RosellM <charlotte0194@gmail.com>
2015-08-03 01:28:30 -0500

    Add and delete estados

---
+++ Mantenimiento/Controllers/estadoController.cs
@@ -0,0 +1,54 @@
+﻿using Entidades;
+using Mantenimiento.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mantenimiento.Controllers
+{
+    public class estadoController : Controller
+    {
+        //
+        // GET: /estado/
+
+        public ActionResult Index()
+        {
+            estadoModel estado = new estadoModel();
+
+            return View(estado);
+        }
+
+        public ActionResult Nuevo() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(estadoModel estado) 
+        {
+            if (ModelState.IsValid) 
+            {
+                estados est = new estados();
+                est.nombre = estado.nombre;
+                est.descripcion = estado.descripcion;
+                new estadoModel().nuevoEstado(est);
+                return View("Index", new estadoModel());
+            }
+            else{
+                return View("Nuevo");
+            }
+
+          
+        }
+
+        public ActionResult eliminarEstado(int id) 
+        {
+
+            new estadoModel().eliminarEstado(id);
+            return View("Index",new estadoModel());
+        }
+
+    }
+}
---
+++ Mantenimiento/Models/estadoModel.cs
@@ -0,0 +1,40 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Negocio;
+
+namespace Mantenimiento.Models
+{
+    public class estadoModel
+    {
+        int exito = 0;
+        public int  id{ get; set; }
+
+        [Display(Name = "NOMBRE DEL NUEVO ESTADO")]
+        [Required(ErrorMessage="Campos requeridos")]
+        public string nombre{ get; set; }
+        [Display(Name = "DESCRIPCION DEL NUEVO ESTADO")]
+        [Required(ErrorMessage="Campos requeridos")]
+        public string descripcion { get; set; }
+
+        public List<estados> listarEstados() 
+        {
+            return new estadoNeg().listarEstados();
+        }
+
+        public void nuevoEstado(estados estado) 
+        {
+            new estadoNeg().nuevoEstado(estado);
+        }
+
+        public void eliminarEstado(int id) 
+        {
+            exito =  new estadoNeg().eliminarEstado(id);
+        }
+
+
+    }
+}
