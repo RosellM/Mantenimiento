16fdf7d3bd89b255c45eb2f6e93aadefcdbf480c
RosellM <charlotte0194@gmail.com>
2015-08-04 02:19:11 -0500

    Index con ciudades y combos con estados

---
+++ Mantenimiento/Controllers/ciudadController.cs
@@ -0,0 +1,31 @@
+﻿using Mantenimiento.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mantenimiento.Controllers
+{
+    public class ciudadController : Controller
+    {
+        //
+        // GET: /ciudad/
+
+        public ActionResult Index()
+        {
+            
+            return View(new ciudadModel());
+        }
+
+        public ActionResult nuevaCiudad() 
+        {
+            return View(new ciudadModel());
+        }
+
+        public ActionResult nuevaCiudad(ciudadModel ciudad)
+        {
+            return View(new ciudadModel());
+        }
+    }
+}
---
+++ Mantenimiento/Models/ciudadModel.cs
@@ -0,0 +1,56 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Negocio;
+using System.Web.Mvc;
+
+namespace Mantenimiento.Models
+{
+    public class ciudadModel
+    {
+        public int id { get; set; }
+        public int idEstado { get; set; }
+        public string nombreEstado { get; set; }
+        [Required(ErrorMessage="Campo requerido")]
+        public string nombre { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        public string descripcion { get; set; }
+
+        public void guardarCiudad(ciudad ciudad)
+        {
+
+            new ciudadesNeg().guardarCiudad(ciudad);
+
+        }
+
+        public List<ciudad> listarCiudad() 
+        {
+
+            return new ciudadesNeg().listarCiudades();
+        }
+
+        public estados obtenerNombreEstado(int idEstado) 
+        {
+            return new ciudadesNeg().nombreEstado(idEstado);
+        }
+
+        public IEnumerable<SelectListItem> estados() 
+        {
+            List<SelectListItem> listItems = new List<SelectListItem>();
+            foreach (var i in new ciudadesNeg().estados()) 
+            {
+                listItems.Add(new SelectListItem
+                {
+                    Text = i.nombre,
+                    Value = i.id.ToString()
+                });
+            }
+
+            return listItems;
+            
+        }
+    }
+}
